cdbac52b36fdf0df10b292a97bf006a2d3121720
MZ <dr.promohands@gmail.com>
2024-08-31 23:37:01 +0500

    5

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,8 +5,13 @@ import AddPost from "./AddPost";
 import Model from "./Modal";
 
 function PostsList() {
+  const [modalIsOpen, setModalIsOpen] = useState(true);
   const [nameChange, setNameChange] = useState("");
   const [locationChange, setLocationChange] = useState("");
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
 
   function changeNameHandler(event) {
     setNameChange(event.target.value);
@@ -16,12 +21,14 @@ function PostsList() {
   }
   return (
     <>
-      <Model>
-        <AddPost
-          onNameChange={changeNameHandler}
-          onLocationChange={changeLocationHandler}
-        />
-      </Model>
+      {modalIsOpen && (
+        <Model onClose={closeModalHandler}>
+          <AddPost
+            onNameChange={changeNameHandler}
+            onLocationChange={changeLocationHandler}
+          />
+        </Model>
+      )}
       <ul className={classes.posts}>
         <Post name={nameChange} location={locationChange} />
         <Post name={"Max"} location={"New York"} />
