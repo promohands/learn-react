2a300f0ba7dca1c29170145ee7b234fe98e3a923
MZ <dr.promohands@gmail.com>
2024-09-01 10:38:21 +0500

    29

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
 import classes from "./AddPost.module.css";
 
-function AddPost({ onNameChange, onLocationChange, onCancel }) {
+function AddPost({ onCancel }) {
+  const [nameEntered, setNameEntered] = useState("");
+  const [locationEntered, setLocationEntered] = useState("");
+
+  function nameChangeHandler(event) {
+    setNameEntered(event.target.value);
+  }
+  function locationChangeHandler(event) {
+    setLocationEntered(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const postData = { name: nameEntered, location: locationEntered };
+    console.log(postData);
+    onCancel();
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" required onChange={onNameChange} />
+        <input type="text" id="name" required onChange={nameChangeHandler} />
       </p>
       <p>
         <label htmlFor="location">Location</label>
-        <input type="text" id="location" required onChange={onLocationChange} />
+        <input
+          type="text"
+          id="location"
+          required
+          onChange={locationChangeHandler}
+        />
       </p>
       <p className={classes.actions}>
         <button type="button" onClick={onCancel}>
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,32 +1,17 @@
-import { useState } from "react";
 import classes from "./PostsList.module.css";
 import Post from "./Post";
 import AddPost from "./AddPost";
 import Model from "./Modal";
 
 function PostsList({ isPosting, onStopPosting }) {
-  const [nameChange, setNameChange] = useState("");
-  const [locationChange, setLocationChange] = useState("");
-
-  function changeNameHandler(event) {
-    setNameChange(event.target.value);
-  }
-  function changeLocationHandler(event) {
-    setLocationChange(event.target.value);
-  }
   return (
     <>
       {isPosting && (
         <Model onClose={onStopPosting}>
-          <AddPost
-            onNameChange={changeNameHandler}
-            onLocationChange={changeLocationHandler}
-            onCancel={onStopPosting}
-          />
+          <AddPost onCancel={onStopPosting} />
         </Model>
       )}
       <ul className={classes.posts}>
-        <Post name={nameChange} location={locationChange} />
         <Post name={"Max"} location={"New York"} />
       </ul>
     </>
